c0abbe58fe20f90d2436cd084a8aa37265db22fd
Meet shah <132123415+Meet6338-X@users.noreply.github.com>
2024-04-15 20:22:43 +0530

    java

--- pr28basic.java
+++ pr28basic.java
@@ -5,6 +5,21 @@ public class pr28basic extends Applet
 {
     public void paint(Graphics g)
     {
-        g.drawString("Hello Aashu ", 120, 120);
-    }
+        g.drawString("Hello Aashu ", 120, 150);
+		for(int i=0;i<=3;i++)
+		{
+			if(i==1)
+			{		
+				g.drawLine(10,20,30,40);
+			}
+			if(i==2)
+			{
+				g.drawOval(100,100,100,100);
+			}
+			if(i==3)
+			{
+				g.drawRect(200,200,80,80);
+			}		
+		}
+	}
 }
